2b20297aa43df81f9b7ad722842a9a9b52814cf7
manuas2008 <manuas2008@gmail.com>
2019-02-16 18:19:44 -0800

    Added test case 2

--- tests/test_power_of_2.py
+++ tests/test_power_of_2.py
@@ -1,10 +1,10 @@
 from PythonSamplePrograms.power_of_2 import power_of_2
 
-def test_power_of_2():
+def test_input_64():
     assert power_of_2(64) == True
 
-def test_power_of_2():
+def test_input_218():
     assert power_of_2(218) == False
 
-def test_power_of_2():
+def test_input_2():
     assert power_of_2(2) == True
