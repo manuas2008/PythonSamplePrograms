af0275351f0e2f75e72441b6d6b1ff1ab1c11109
manuas2008 <manuas2008@gmail.com>
2025-12-02 13:08:57 -0800

    Delete tests/test_power_of_4.py

--- tests/test_power_of_4.py
+++
@@ -1,7 +0,0 @@
-from PythonSamplePrograms.power_of_4 import power_of_4
-
-def test_power_of_4_input_64():
-    assert power_of_4(64) == True
-
-def test_power_of_4_input_5():
-    assert power_of_4(5) == False
