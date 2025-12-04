d22861a968eb85ac974940c7cbeeb330b08ed0de
manuas2008 <manuas2008@gmail.com>
2025-12-04 14:11:13 -0800

    Bug fix

--- PythonSamplePrograms/base7.py
+++ PythonSamplePrograms/base7.py
@@ -1,4 +1,8 @@
 def convertToBase7(num: int) -> int:
+    negative = 1
+    if num < 0:
+        negative = -1
+        num = abs(num)
     base7 = 0
     ten_pow = 0
     while num >= 7:
@@ -6,7 +10,7 @@ def convertToBase7(num: int) -> int:
         num //= 7
         ten_pow += 1
     base7 += 10 ** ten_pow * num
-    return base7
+    return base7 * negative
 
 print(convertToBase7(10))
 print(convertToBase7(72))
