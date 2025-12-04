f6092db88ebf914198631a221ccdfd2eb4430fce
manuas2008 <manuas2008@gmail.com>
2025-12-04 11:21:38 -0800

    Update test_three_sum.py

--- tests/test_three_sum.py
+++ tests/test_three_sum.py
@@ -4,4 +4,4 @@ def test_three_sum_positive():
     assert (three_sum([-1,0,1,2,-1,-4]) == [[-1, -1, 2], [-1, 0, 1]])
 
 def test_two_sum_negative():
-    assert(three_sum([1,2,3]), []
+    assert(three_sum([1,2,3]), [])
