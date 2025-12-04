949930bc03b1baea0bb4158a73f19edca56ddb21
manuas2008 <manuas2008@gmail.com>
2025-12-04 11:19:54 -0800

    Create test_three_sum.py

---
+++ tests/test_three_sum.py
@@ -0,0 +1,7 @@
+from PythonSamplePrograms.three_sum import three_sum
+
+def test_three_sum_positive():
+    assert (three_sum([-1,0,1,2,-1,-4]) == [[-1, -1, 2], [-1, 0, 1]])
+
+def test_two_sum_negative():
+    assert(three_sum([1,2,3]), []
