8c0ce6c3a1917df01b88bd87ee963f18f7b66e8c
manuas2008 <manuas2008@gmail.com>
2025-12-31 12:25:01 -0800

    Added sort012.py

---
+++ tests/test_sort012.py
@@ -0,0 +1,4 @@
+from PythonSamplePrograms.sort012 import sort012
+
+def test_sort012_positive():
+    assert(sort012([2, 2, 1, 0, 1, 2, 1, 0, 1, 2, 0, 1, 2]) == [0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2])
